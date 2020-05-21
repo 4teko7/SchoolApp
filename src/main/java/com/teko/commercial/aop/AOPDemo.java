12050335afff057ebc3108e1df17ae68a3137a80
4teko7 <bilal.tekin@boun.edu.tr>
2020-05-21 22:59:37 +0300

    UserRoleRepoServiceEntityWereAdded2CommitBeforeAndSomePointcutAnnotationExamplesWereAlsoAdded

--- src/main/java/com/teko/commercial/aop/AOPDemo.java
+++ src/main/java/com/teko/commercial/aop/AOPDemo.java
@@ -2,6 +2,7 @@ package com.teko.commercial.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +18,21 @@ public class AOPDemo {
 	
 	//@Before Advide
 	
-//	@Before("execution(public String com.teko.commercial.controllers.HomeController.root())")
-	@Before("execution(* root*(..))")
+
+//	@Before("execution(* root*(..))") //this okay
+
+	
+//	@Before("execution(public String com.teko.commercial.controllers.HomeController.root(..))")  /this is okay
+	
+//	@Before("execution(public String com.teko.commercial.controllers.HomeController.root(..,org.springframework.ui.Model))") //This is for parameters
+	
+//	@Before("execution(public String com.teko.commercial.controllers.HomeController.root(boolean,org.springframework.ui.Model))") //This works also.
+	
+	
+	@Pointcut("execution(public String com.teko.commercial.controllers.HomeController.root(boolean,org.springframework.ui.Model))")
+	private void forRootPointCut() {}
+	
+	@Before("forRootPointCut()")
 	public void beforeHomeAdvice() {
 		
 		EncodeDecode en = new EncodeDecode();
@@ -26,6 +40,11 @@ public class AOPDemo {
 		System.out.println(en.encode("merhaba"));
 		System.out.println(en.decode(en.encode("merhaba")));
 		System.out.println("BEFORE HOME WAS EXECUTED !");
+	}
+	
+	@Before("forRootPointCut()")
+	public void sayHello() {
+		System.out.println("HELLO FROM SAY HELLO");
 	}
 	
 }
